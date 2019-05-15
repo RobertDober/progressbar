c63b07e459a6ed693abeb6dc4f16a2963a5715b3
RobertDober <robert.dober@gmail.com>
2019-05-15 08:59:31 +0200

    initial

---
+++ lib/progressbar.ex
@@ -0,0 +1,18 @@
+defmodule Progressbar do
+  @moduledoc """
+  Documentation for Progressbar.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Progressbar.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/progressbar_test.exs
@@ -0,0 +1,8 @@
+defmodule ProgressbarTest do
+  use ExUnit.Case
+  doctest Progressbar
+
+  test "greets the world" do
+    assert Progressbar.hello() == :world
+  end
+end
